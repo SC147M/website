2e6d796e12360bcbe036d2a3f33e600ac5c98692
Christian Lorenz <christian.lorenz.hh@gmail.com>
2021-03-14 14:53:15 +0100

    deactivated registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -28,6 +28,8 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
     {
+        die("zur Zeit sind keine Registrierungen möglich");
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
