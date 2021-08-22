32fc9a53212ac27542e15a2ed3aa53cce2207a13
Markus Kraus <markus.kraus@anzing.net>
2021-08-22 20:32:13 +0200

    Teams 2021/22 online

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -87,14 +87,6 @@ class IndexController extends AbstractController
     {
         return $this->render('index/teams.html.twig');
     }
-
-    /**
-     * @Route("/teamstest", name="teamstest")
-     */
-    public function teamstest()
-    {
-        return $this->render('index/teams-test.html.twig');
-    }
     
     /**
      * @Route("/breaks_old", name="breaks_old")
