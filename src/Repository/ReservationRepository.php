18308f8302edc0d35f957684cdcc3e1c55de03c6
Markus Kraus <markus.kraus@anzing.net>
2023-03-05 12:03:00 +0100

    exclude outward games table reservations in UI

--- src/Repository/ReservationRepository.php
+++ src/Repository/ReservationRepository.php
@@ -53,9 +53,6 @@ class ReservationRepository extends ServiceEntityRepository
         if (is_null($end)) {
             $end = new DateTime('9999-12-31 23:59:59');
         }
-        //const OUTWARDS_TABLE = 4;
-        //    ->andWhere(':outwards NOT IN(r.tables)')
-        //    ->setParameter('outwards', OUTWARDS_TABLE)
 
         return $this->createQueryBuilder('r')
             ->andWhere('r.end > :start')
