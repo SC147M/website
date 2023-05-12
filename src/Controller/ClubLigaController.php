ed772f193e201265d800c643ed31daf72dfaaa61
Markus Kraus <markus.kraus@anzing.net>
2023-05-12 11:27:35 +0200

    Undo standings

--- src/Controller/ClubLigaController.php
+++ src/Controller/ClubLigaController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\ClubLigaMatch;
 use App\Repository\ClubLigaMatchRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,12 +20,6 @@ class ClubLigaController extends AbstractController
         return $this->render('club_liga_new/index.html.twig', [
             'stand'     => '5.1.2023',
             'matches'   => $matches,
-            'standings' => $standings,
         ]);
-    }
-
-    private function getStandings(ClubLigaMatch[] $matches)
-    {
-        return [];
     }
 }
